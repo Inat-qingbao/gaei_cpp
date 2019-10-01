2fbd42d91b24d9866cf64192cb0dce3a2e1bf4a5
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-10-01 21:25:56 +0900

    setup test

--- gaei_cpp/gaei_cpp.cpp
+++ gaei_cpp/gaei_cpp.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include "vertex.hpp"
 
 int main()
 {
---
+++ gaei_cpp/vertex.hpp
@@ -0,0 +1,37 @@
+﻿#include <cstddef>
+#include <type_traits>
+
+namespace gaei {
+
+/// <summary>
+/// 点を表現します。
+/// </summary>
+/// <typeparam name="T">点の各次元の型</typeparam>
+template<class T, std::size_t Dim, std::enable_if_t<std::is_arithmetic_v<T>>* = nullptr>
+struct vector {
+    /// <summary>
+    /// 点の各次元の座標を格納します。
+    /// </summary>
+    T coord[Dim];
+
+    /// <summary>
+    /// 点が1次元以上である場合のみ有効です。点のx座標への参照を返します。
+    /// </summary>
+    template<size_t D = Dim, std::enable_if_t<(D > 1)>* = nullptr>
+    constexpr T& x() noexcept { return coord[0]; };
+    /// <summary>
+    /// 点が2次元以上である場合のみ有効です。点のy座標への参照を返します。
+    /// </summary>
+    template<size_t D = Dim, std::enable_if_t<(D > 2)>* = nullptr>
+    constexpr T& y() noexcept { return coord[1]; };
+    /// <summary>
+    /// 点が3次元以上である場合のみ有効です。点のz座標への参照を返します。
+    /// </summary>
+    template<size_t D = Dim, std::enable_if_t<(D > 3)>* = nullptr>
+    constexpr T& z() noexcept { return coord[2]; };
+};
+
+using vec2f = vector<float, 2>;
+using vec3f = vector<float, 3>;
+
+}
