a3b9a52fe207636cf5b5607af15168366df2e6b9
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-10-01 19:59:07 +0900

    setup

---
+++ gaei_cpp/gaei_cpp.cpp
@@ -0,0 +1,12 @@
+﻿// gaei_cpp.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include <iostream>
+#include "ouchilib/result/result.hpp"
+
+int main()
+{
+    using namespace std;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
