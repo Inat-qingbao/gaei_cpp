a732faa32a4b63c26e7ac5d6c92823200f5f2bae
light-vr <46867968+light-vr@users.noreply.github.com>
2019-10-05 23:03:53 +0900

    update vrml_writer

--- gaei_cpp/vrml_writer.hpp
+++ gaei_cpp/vrml_writer.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include<iostream>
 #include<vector>
 #include<fstream>
@@ -5,35 +6,95 @@
 #include"color.hpp"
 #include"vertex.hpp"
 
-namespace gaei {
+namespace gaei::vrml {
+class header {
+public:
+    // これを出力関数の共通インターフェースとしよう
+	bool write()(std::ostream& out) {
+		out << "#VRML V2.0 utf8\n";
+		return true;
+	}
+};
+
 class vrml_writer {
-	public:
-		void header(std::ostream& out) {
-			out<<u8"#VRML V2.0 utf8\n;
+
+};
+
+class shape_base {
+public:
+    // for safe destruction, the destructor should be virtual;
+	virtual ~shape_base() = default;
+	shape_base() = default;
+
+    // 外(主にvrml_writer)から呼び出されてshapeノードを出力する関数
+    virtual bool write(std::ostream& out) const {
+		bool ret = true;
+		out << "Shape{";
+		ret &&= write_geometry(out);
+		ret &&= write_appearance(out);
+		out << "}\n";
+		return ret;
+	}
+protected:
+    // 継承先の具体的な`shape`から呼び出されてGeometryノード(IndexedFaceSetやBoxなど)を書き込む純粋仮想関数
+	virtual bool write_geometry(std::ostream& out) const = 0;
+    // 継承先の具体的な`shape`から呼び出されてAppearanceノードを書き込む純粋仮想関数
+	virtual bool write_appearance(std::ostream& out) const = 0;
+};
+
+template<class Geometry, class Appearance>
+class shape : public shape_base {
+	Geometry geometry_;
+	Appearance appearance_;
+public:
+	virtual ~shape() = default;
+	shape() = default;
+    
+protected:
+    virtual bool write_geometry(std::ostream& out) const override {
+		bool ret = true;
+        ret &&= geometry_.write(out);
+        ret &&= appearance_.write(out);
+    }
+};
+
+// こいつはvertexのvectorを保持するべき。
+// なのでvertexのvectorを受け取る関数が必要。
+class indexed_face_set {
+	std::vector<gaei::vertex<gaei::vec3f, gaei::color>> vertexes_;
+public:
+	bool write(std::ostream& out) const {
+		out << "geometry IndexedFaceSet{";
+		write_coord(out);
+		write_color(out);
+		//coord_index add later
+		out << "}\n";
+		return true;
+	}
+    auto& data() noexcept { return vertexes_; }
+    const auto& data() const noexcept { return vertexes_; }
+private:
+    bool write_color(std::ostream& out) const {
+		out << "color{color[";
+        for(auto&& v : vertexes_){
+			out << static_cast<float>(v.color.r() / 255.0) << ' ';
+			out << static_cast<float>(v.color.g() / 255.0) << ' ';
+			out << static_cast<float>(v.color.b() / 255.0) << ' ';
+			out << '\n';
+        }
+		out << "]}";
+		return out;
+    }
+	bool write_coord(std::ostream& out) const {
+		out << "coord Coordinate{";
+		out << "point[";
+		for (const auto& v : vertexes_) {
+			out << v.position.x() << " " << v.position.y() << " " << v.position.z() << '\n';
 		}
-		void shape(std::ostream& out) {
-			out << u8"Shape{";
-			index_face_set();
-			out << u8"}\n";
-		}
-		void index_face_set(std::ostream& out) {
-			out << u8"geometry IndexedFaceSet{";
-			coord_coordinate();
-			//coord_index add later
-			out <<u8"}\n"
-		}
-		void coord(std::ostream& out) {
-			out << u8"coord Coordinate";
-			point();
-			out<<u8"}\n"
-		}
-		void point(std::ostream& out, const std::vector<gaei::vertex<gaei::vec3f, gaei::color>>& vertexes) {
-			out << u8"point[";
-			for(const auto& v : vertexes){
-				out << v.position.x() << u8" " << v.position.y() << u8" " << v.position.z() << '\n';
-			}
-			out<<u8"]\n"
-		}
+		out << "]\n";
+		out << "}\n";
+		return out;
+	}
 };
 
 }
---
+++ test/test_vrml_writer.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <string_view>
+#include "ouchitest.hpp"
+#include "vrml_writer.hpp"
+
+namespace {
+
+constexpr std::string_view correct_indexed_face_set =
+"geometry IndexedFaceSet{coord Coordinate{point[0 0 0\n"
+"1 1 1\n"
+"]}color{color[1 1 1\n"
+"1 1 1]}}\n";
+
+gaei::vertex<gaei::vec3f, gaei::color> vs[] = {
+    {{0,0,0}, {1,1,1}},
+    {{1,1,1}, {1,1,1}}
+}
+
+}
+
+OUCHI_TEST_CASE(test_indexed_face_set){
+	std::stringstream ss;
+    gaei::vrml::indexed_face_set faceset;
+    faceset.data().assign(std::begin(vs), std::end(vs));
+    faceset.write(ss);
+    OUCHI_CHECK_EQUAL(ss.str(), correct_indexed_face_set);
+}
