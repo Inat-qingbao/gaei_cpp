7436f3116b635dc67bdbe4532bae596af0395ad2
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-10-15 10:44:48 +0900

    Update test_vertex.cpp

--- test/test_vertex.cpp
+++ test/test_vertex.cpp
@@ -7,3 +7,21 @@ OUCHI_TEST_CASE(test_vector_instantiate)
     v2.y() = 0;
     OUCHI_CHECK_EQUAL(v2.coord[1], 0);
 }
+
+OUCHI_TEST_CASE(test_vector_order)
+{
+    {
+        gaei::vec2f v1{ 0, 100 };
+        gaei::vec2f v2{ 1, 0 };
+        OUCHI_CHECK_TRUE(v1 < v2);
+        OUCHI_CHECK_TRUE(v2 > v1);
+        OUCHI_CHECK_TRUE(v1 != v2);
+    }
+    {
+        gaei::vec2f v1{ 2, 10 };
+        gaei::vec2f v2{ 2,20 };
+        OUCHI_CHECK_TRUE(v1 < v2);
+        OUCHI_CHECK_TRUE(v2 > v1);
+        OUCHI_CHECK_TRUE(v1 != v2);
+    }
+}
