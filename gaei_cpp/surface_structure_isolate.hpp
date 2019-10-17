561afdde275da3943729445b220d3b03ed7d6568
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-10-17 21:45:12 +0900

    dfs->bfs ( close #14 )

--- gaei_cpp/surface_structure_isolate.hpp
+++ gaei_cpp/surface_structure_isolate.hpp
@@ -9,7 +9,6 @@
 
 #include "vertex.hpp"
 #include "color.hpp"
-#include "ouchilib/result/result.hpp"
 
 namespace gaei {
 
@@ -18,57 +17,57 @@ public:
     static constexpr std::uint32_t border = 1u << 31;
     surface_structure_isolate(float diff = 4)
         : not_visited_{}
-        , vertexes_{ nullptr }
         , diff_{ diff }
-        , color_{ 0 }
     {}
     /// <remarks>
     /// vertexesは変更されないが、要素を変更するためconst参照ではない。
     /// </remarks>
     auto operator() (std::vector<vertex<>>& vertexes)
     {
-        color_ = 0;
-        vertexes_ = &vertexes;
+        std::uint32_t color = 0;
         for (auto it = vertexes.begin(); it != vertexes.end(); ++it) {
             not_visited_.insert(it);
         }
         while (not_visited_.size()) {
-            visit(*not_visited_.begin(), color_++);
+            visit(*not_visited_.begin(), vertexes, color++);
         }
-        vertexes_ = nullptr;
-        return color_;
+        return color;
     }
 private:
     std::set<std::vector<vertex<>>::iterator> not_visited_;
-    std::vector<vertex<>>* vertexes_;
     float diff_;
-    std::uint32_t color_;
 
-    void visit(std::vector<vertex<>>::iterator t, std::uint32_t label) noexcept
+    void visit(std::vector<vertex<>>::iterator t, std::vector<vertex<>>& vs, std::uint32_t label) noexcept
     {
         constexpr vec2f d[4] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
-        vec2f tv{ t->position.x(), t->position.y() };
-        // 訪問済みのしるしをつける
-        t->color = color{ label };
-        // 未訪問リストから消す
-        not_visited_.erase(t);
-        // 探索は4方向に伸びていく
-        for (auto&& dv : d) {
-            vec2f nv = tv + dv;
-            auto [nitr, unused] = std::equal_range(vertexes_->begin(), vertexes_->end(), nv,
-                                                   overloaded{
-                                                   [](const vertex<>& vv, const vec2f& v) {return vv.position < v; },
-                                                   [](const vec2f& v, const vertex<>& vv) {return v < vv.position; }
-                                                   });
+        std::queue<std::vector<vertex<>>::iterator> queue;
+        queue.push(t);
+        while (queue.size()) {
+            auto ot = queue.front();
+            queue.pop();
+            // 訪問済みのしるしをつける
+            ot->color = color{ label };
+            // 未訪問リストから消す
+            not_visited_.erase(ot);
+            vec2f tv{ ot->position.x(), ot->position.y() };
+            // 探索は4方向に伸びていく
+            for (auto&& dv : d) {
+                vec2f nv = tv + dv;
+                auto [nitr, unused] = std::equal_range(vs.begin(), vs.end(), nv,
+                                                       overloaded{
+                                                       [](const vertex<>& vv, const vec2f& v) {return vv.position < v; },
+                                                       [](const vec2f& v, const vertex<>& vv) {return v < vv.position; }
+                                                       });
 
-            // nvの位置に要素がないか、すでに訪問済みならば次の探索候補を見る
-            if (nitr == vertexes_->end() || nitr->color.is_valid()) continue;
-            // nvの位置の要素に別のラベルを付けるべきなら境界印をつけて次の探索候補を見る
-            if (std::abs(nitr->position.z() - t->position.z()) > diff_) {
-                t->color = color{ label | border };
-                continue;
+                // nvの位置に要素がないか、すでに訪問済みならば次の探索候補を見る
+                if (nitr == vs.end() || nitr->color.is_valid()) continue;
+                // nvの位置の要素に別のラベルを付けるべきなら境界印をつけて次の探索候補を見る
+                if (std::abs(nitr->position.z() - ot->position.z()) > diff_) {
+                    ot->color = color{ label | border };
+                    continue;
+                }
+                queue.push(nitr);
             }
-            visit(nitr, label);
         }
     }
 };
