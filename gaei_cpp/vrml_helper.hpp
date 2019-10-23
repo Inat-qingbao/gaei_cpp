559c7ec9826b9de49a8239a91b29e5a1af124795
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-10-23 16:02:54 +0900

    Update vrml_helper.hpp

--- gaei_cpp/vrml_helper.hpp
+++ gaei_cpp/vrml_helper.hpp
@@ -15,19 +15,16 @@ namespace gaei::vrml {
 /// <returns>
 /// 成功した場合書き込んだ文字列の長さ。失敗した場合エラーコード
 /// </returns>
-ouchi::result::result<std::size_t, std::errc>
-to_vrml(color val, char* first, char* last) noexcept
+inline ouchi::result::result<std::size_t, std::errc>
+inline to_vrml(color val, char* first, char* last) noexcept
 {
     std::to_chars_result r;
-    assert(first < last);
     r = std::to_chars(first, last, val.rf());
     *(r.ptr) = ' ';
     ++(r.ptr);
-    assert(r.ptr < last);
     r = std::to_chars(r.ptr, last, val.gf());
     *(r.ptr) = ' ';
     ++(r.ptr);
-    assert(r.ptr < last);
     r = std::to_chars(r.ptr, last, val.bf());
     *(r.ptr) = 0;
     if (r.ec != std::errc{}) return ouchi::result::err(r.ec);
@@ -35,7 +32,7 @@ to_vrml(color val, char* first, char* last) noexcept
 }
 
 template<size_t InternalBufferLength = 64>
-ouchi::result::result<std::size_t, std::errc>
+inline ouchi::result::result<std::size_t, std::errc>
 to_vrml(color val, std::string& dest)
 {
     char buffer[InternalBufferLength] = {};
@@ -44,28 +41,32 @@ to_vrml(color val, std::string& dest)
     return r;
 }
 
-ouchi::result::result<std::size_t, std::errc>
-to_vrml(const vec3f& val, char* first, char* last) noexcept
+namespace detail {
+
+template<class T, size_t D, size_t ...S>
+inline ouchi::result::result<std::size_t, std::errc>
+vec_unfold(const vector<T, D>& val, char* first, char* last, std::index_sequence<S...>) noexcept
 {
     std::to_chars_result r;
-    assert(first < last);
-    r = std::to_chars(first, last, val.x());
-    *(r.ptr) = ' ';
-    ++(r.ptr);
-    assert(r.ptr < last);
-    r = std::to_chars(r.ptr, last, val.y());
-    *(r.ptr) = ' ';
-    ++(r.ptr);
-    assert(r.ptr < last);
-    r = std::to_chars(r.ptr, last, val.z());
-    *(r.ptr) = 0;
+    r.ptr = first;
+    (((r = std::to_chars(r.ptr, last, val.coord[S])), *(r.ptr++) = ' '), ...);
     if (r.ec != std::errc{}) return ouchi::result::err(r.ec);
     return ouchi::result::ok(first - r.ptr - 1);
 }
 
-template<size_t InternalBufferLength = 64>
-ouchi::result::result<std::size_t, std::errc>
-to_vrml(const vec3f& val, std::string& dest)
+}
+
+template<class T, size_t S>
+inline ouchi::result::result<std::size_t, std::errc>
+to_vrml(const vector<T, S>& val, char* first, char* last) noexcept
+{
+    std::to_chars_result r;
+    return detail::vec_unfold(val, first, last, std::make_index_sequence<S>{});
+}
+
+template<class T, size_t S, size_t InternalBufferLength = 64>
+inline ouchi::result::result<std::size_t, std::errc>
+to_vrml(const vector<T, S>& val, std::string& dest)
 {
     char buffer[InternalBufferLength] = {};
     auto r = to_vrml(val, buffer, buffer + InternalBufferLength);
