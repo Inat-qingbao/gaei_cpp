aee888fee3d2fe73053f2e94e1969404af811306
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-10-25 10:03:40 +0900

    削減率49.1%

--- gaei_cpp/reduce_points.hpp
+++ gaei_cpp/reduce_points.hpp
@@ -16,14 +16,21 @@ std::vector<size_t> count_label(size_t label_size, const std::vector<vertex<>>& 
     return std::move(lc);
 }
 
-void remove_trivial_surface(size_t label_size, std::vector<vertex<>>& vs)
+void remove_trivial_surface(const std::vector<size_t>& lc, std::vector<vertex<>>& vs)
 {
     using ssi = surface_structure_isolate;
-    auto lc = count_label(label_size, vs);
     auto max = std::distance(lc.cbegin(), std::max_element(lc.cbegin(), lc.cend()));
     vs.erase(std::remove_if(vs.begin(), vs.end(),
                             [max](const vertex<>& v) { return v.color.value() == max; }),
              vs.end());
 }
+
+void remove_minor_labels(std::vector<size_t> lc, std::vector<vertex<>>& vs, size_t threshold = 5) noexcept
+{
+    vs.erase(std::remove_if(vs.begin(), vs.end(),
+                            [threshold, &lc](const vertex<>& v) { return lc[color_to_idx(v.color.value())] < threshold; }),
+             vs.end());
+}
+
 
 }
