069e4b011bf0e9ce152f523c0cf782e7f62265b5
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-11-04 13:18:22 +0900

    Update surface_structure_isolate.hpp

--- gaei_cpp/surface_structure_isolate.hpp
+++ gaei_cpp/surface_structure_isolate.hpp
@@ -5,13 +5,21 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <set>
 #include <queue>
 
 #include "vertex.hpp"
 #include "color.hpp"
-#include "ouchilib/crypto/algorithm/aes.hpp"
 
 namespace gaei {
+
+// MSVCのabs関数がinline指定されていないため。
+template<class T>
+constexpr auto abs(T a) noexcept
+-> std::enable_if_t<std::is_arithmetic_v<T>, T>
+{
+    return a < 0 ? -a : a;
+}
 
 // これでカラフルになる
 constexpr std::uint32_t idx_to_color(unsigned int idx) noexcept
@@ -35,7 +43,7 @@ public:
     /// vertexesは変更されないが、要素を変更するためconst参照ではない。
     /// </remarks>
     template<class C>
-    auto operator() (C&& vertexes)
+    auto operator()(C&& vertexes)
     {
         std::uint32_t color = 0;
         for (auto it = vertexes.begin(); it != vertexes.end(); ++it) {
@@ -48,40 +56,38 @@ public:
     }
 private:
     std::set<std::vector<vertex<>>::iterator> not_visited_;
+    std::queue<std::vector<vertex<>>::iterator> queue_;
     float diff_;
+    static constexpr vec2f d[4] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+
 
     template<class Itr, std::enable_if_t<std::is_same_v<vertex<>, typename std::iterator_traits<Itr>::value_type>>* = nullptr>
-    void visit(Itr t, Itr first, Itr last, std::uint32_t label) noexcept
+    void visit(Itr t, Itr first, Itr last, std::uint32_t label)
     {
-        constexpr vec2f d[4] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
-        std::queue<Itr> queue;
-        queue.push(t);
-        while (queue.size()) {
-            auto ot = queue.front();
-            queue.pop();
+        queue_.push(t);
+        while (queue_.size()) {
+            auto ot = queue_.front();
+            queue_.pop();
             // 訪問済みのしるしをつける
             ot->color = color{ label };
             // 未訪問リストから消す
             not_visited_.erase(ot);
-            vec2f tv{ ot->position.x(), ot->position.y() };
+            const vec2f tv{ ot->position.x(), ot->position.y() };
             // 探索は4方向に伸びていく
             for (auto&& dv : d) {
-                vec2f nv = tv + dv;
-                auto nitr = std::lower_bound(first, last, nv,
-                                             overloaded{
-                                             [](const vertex<>& vv, const vec2f& v) {return vv.position < v; },
-                                             [](const vec2f& v, const vertex<>& vv) {return v < vv.position; }
-                                             });
+                const vec2f nv = tv + dv;
+                const auto nitr = std::lower_bound(first, last, nv,
+                                                   [](const vertex<>& vv, const vec2f& v) {return vv.position < v; });
                 // nvの位置に要素がないならば境界印を付けて次の探索候補を見る
                 // nvの位置の要素に別のラベルを付けるべきなら境界印をつけて次の探索候補を見る
-                if (nitr->position != nv || std::abs(nitr->position.z() - ot->position.z()) > diff_) {
+                if (nitr->position != nv || abs(nitr->position.z() - ot->position.z()) > diff_) {
                     ot->color = color{ label | border };
                     continue;
                 }
                 // すでに訪問済みならば次の探索候補を見る
                 if (nitr->color.is_valid()) continue;
                 nitr->color.validate();
-                queue.push(nitr);
+                queue_.push(nitr);
             }
         }
     }
