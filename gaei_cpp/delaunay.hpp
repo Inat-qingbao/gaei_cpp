cd5ffe97286edf4ab83f5bc7b23bda70c2710045
kongou923 <39829359+kongou923@users.noreply.github.com>
2019-11-24 23:21:34 +0900

    Create delaunator.hpp

--- gaei_cpp/delaunay.hpp
+++ gaei_cpp/delaunay.hpp
@@ -2,28 +2,18 @@
 #include<cmath>
 #include<vector>
 #include"vertex.hpp"
+#include"delaunator.hpp"
 
 
 namespace gaei {
-	/*まず最大値と最小値を求める*/
-	const std::vector<gaei::vertex<gaei::vec3f, gaei::color>>& vertexes;
-	/*for (const auto& vertex : vertexes) {
-		vertex.position.x();
-		vertex.position.y();
-		vertex.position.z();
-	}*/
-	double minX = *std::min_element(vertexes.begin(), vertexes.end()) 
-		[](const auto& v1, const auto& v2) { return v1.position.x() < v2.position.x(); });
-	double minY = *std::min_element(vertexes.begin(), vertexes.end())
-		[](const auto& v1, const auto& v2) { return v1.position.y() < v2.position.y(); });
-	double minZ = *std::min_element(vertexes.begin(), vertexes.end())
-		[](const auto& v1, const auto& v2) { return v1.position.z() < v2.position.z(); });
-	double maxX = *std::max_element(vertexes.begin(), vertexes.end())
-		[](const auto& v1, const auto& v2) { return v1.position.x() < v2.position.x(); });
-	double maxY = *std::max_element(vertexes.begin(), vertexes.end())
-		[](const auto& v1, const auto& v2) { return v1.position.y() < v2.position.y(); });
-	double maxZ = *std::max_element(vertexes.begin(), vertexes.end())
-		[](const auto& v1, const auto& v2) { return v1.position.z() < v2.position.z(); });
-
-	
+	void delaunay(std::vector<gaei::vertex<gaei::vec3f, gaei::color>>& vertexes) {
+		std::vector<double> coords;
+		for (const auto& vertex:vertexes) {
+			int i = 0;
+			coords[i] = vertex.position.x();
+			coords[i +1] = vertex.position.y();
+			i += 2;
+		}
+		delaunator::Delaunator d(coords);
+		
 }
