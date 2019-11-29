e5670b26a5e359acb672392c02192120d9f6e2ce
light-vr <46867968+light-vr@users.noreply.github.com>
2019-11-29 23:23:05 +0900

    Update triangle_direction.hpp

--- gaei_cpp/triangle_direction.hpp
+++ gaei_cpp/triangle_direction.hpp
@@ -1,24 +1,22 @@
 ﻿#pragma once
 #include <vector>
-#include <iostream>
 #include <array>
 #include "vertex.hpp"
 #include "vector_utl.hpp"
 
 namespace gaei{
-    
-void triangle_direction_judege(std::vector<gaei::vertex<gaei::vec3f, gaei::color>>& vertexes, std::vector<std::array>& as) {
-    std::vector<double, 3> c, vab, vbc;
-    for (int i = 0; i < as.size; i++) {
-        vab.coord[0] = vertexes[as[i][1]].x() - vertexes[as[i][0]].x();
-        vab.coord[1] = vertexes[as[i][1]].y() - vertexes[as[i][0]].y();
-        vab.coord[2] = vertexes[as[i][1]].z() - vertexes[as[i][0]].z();
-        vbc.coord[0] = vertexes[as[i][2]].x() - vertexes[as[i][1]].x();
-        vbc.coord[1] = vertexes[as[i][2]].y() - vertexes[as[i][1]].y();
-        vbc.coord[2] = vertexes[as[i][2]].z() - vertexes[as[i][1]].z();
+void triangle_direction_judege(std::vector<gaei::vertex<gaei::vec3f, gaei::color>>& vertexes, std::vector<std::array<int,3>>& as) {
+    gaei::vector<double, 3> c, vab, vbc;
+    for (int i = 0; i < as.size(); ++i) {
+        vab.coord[0] = vertexes[as[i][1]].position.x() - vertexes[as[i][0]].position.x();
+        vab.coord[1] = vertexes[as[i][1]].position.y() - vertexes[as[i][0]].position.y();
+        vab.coord[2] = vertexes[as[i][1]].position.z() - vertexes[as[i][0]].position.z();
+        vbc.coord[0] = vertexes[as[i][2]].position.x() - vertexes[as[i][1]].position.x();
+        vbc.coord[1] = vertexes[as[i][2]].position.y() - vertexes[as[i][1]].position.y();
+        vbc.coord[2] = vertexes[as[i][2]].position.z() - vertexes[as[i][1]].position.z();
         c =gaei::cross_product(vab, vbc);
-        auto in = c.coord[2];
-        if (in > 0.0) {
+        double in = c.coord[2];
+        if (in < 0.0) {
             std::swap(as[i][0], as[i][2]);
         }
     }
---
+++ test/test_triangle_direction.cpp
@@ -0,0 +1,18 @@
+﻿#include"triangle_direction.hpp"
+#include"ouchitest.hpp"
+OUCHI_TEST_CASE(test_triangle_direction){
+    std::vector<std::array<int,3>> as;
+    std::vector<gaei::vertex<gaei::vec3f, gaei::color>> vertexes;
+    as.resize(3);
+    as[0][0] = 0;
+    as[0][1] = 1;
+    as[0][2] = 2;
+    std::vector<std::array<int,3>> ans;
+    ans = as;
+    vertexes.resize(3);
+    vertexes[0].position = gaei::vec3f{ {0,0,0} };
+    vertexes[1].position = gaei::vec3f{ {3,0,3} };
+    vertexes[2].position = gaei::vec3f{ {3,1,0} };
+    gaei::triangle_direction_judege(vertexes, as);
+    OUCHI_CHECK_EQUAL(as[0][0],ans[0][0]);
+}
