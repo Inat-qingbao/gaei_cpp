bc26a810292467b47c093d6771d87580659b9df0
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-12-06 18:03:35 +0900

    わからない

--- gaei_cpp/normalize.hpp
+++ gaei_cpp/normalize.hpp
@@ -13,7 +13,7 @@ template<class ExecutionPolicy = std::execution::sequenced_policy>
 void normalize(std::vector<vertex<>>& vs)
 {
     std::mt19937 mt;
-    std::uniform_real_distribution<double> di(1.0e-1f, 1.0e-1f);
+    std::uniform_real_distribution<double> di(1.0e-1, 1.0e-1);
     std::for_each(ExecutionPolicy{}, vs.begin(), vs.end(),
                   [f = vs.front().position, mt, di](vertex<>& v) mutable
     {v.position.x() -= f.x(); v.position.y() -= f.y(); });
--- gaei_cpp/reduce_points.hpp
+++ gaei_cpp/reduce_points.hpp
@@ -50,7 +50,7 @@ inline void thinout(std::vector<vertex<>>& vs) noexcept
     std::sort(vs.begin(), vs.end(),
               [](auto&& a, auto&& b) {return a.position < b.position; });
     vs.erase(std::remove_if(vs.begin(), vs.end(),
-                            [idx = 0u](const vertex<>& v) mutable {return false; }),
+                            [idx = 0u](const vertex<>& v) mutable {return /*v.color.a() != 0 && */((int)v.position.x() % 3 || (int)v.position.y() % 3); }),
              vs.end());
 }
 
